f3aa944565ec44a87110d4789e91bc1651ca2c6c
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2020-10-29 12:07:08 -0700

    pagina principal, aqui se envian los usuarios cuando entran al sistema

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,8 +1,6 @@
-import React, { Component} from 'react';
-import { Bar, Line } from 'react-chartjs-2';
-import Proyects from '../../proyects';
+import React from 'react';
 import {
- 
+
   Card,
   CardBody,
   Col,
@@ -11,259 +9,56 @@ import {
 } from 'reactstrap';
 
 
-const cardChartOpts1 = {
-  tooltips: {
-    enabled: false,
-    // custom: CustomTooltips
-  },
-  maintainAspectRatio: false,
-  legend: {
-    display: false,
-  },
-  scales: {
-    xAxes: [
-      {
-        gridLines: {
-          color: 'transparent',
-          zeroLineColor: 'transparent',
-        },
-        ticks: {
-          fontSize: 2,
-          fontColor: 'transparent',
-        },
 
-      }],
-    yAxes: [
-      {
-        display: false,
-        ticks: {
-          display: false,
-          
-        },
-      }],
-  },
-  elements: {
-    line: {
-      borderWidth: 1,
-    },
-    point: {
-      radius: 4,
-      hitRadius: 10,
-      hoverRadius: 4,
-    },
-  }
-}
+const Dashboard = () => {
 
-
-
-const cardChartOpts2 = {
-  tooltips: {
-    enabled: false
-  },
-  maintainAspectRatio: false,
-  legend: {
-    display: false,
-  },
-  scales: {
-    xAxes: [
-      {
-        gridLines: {
-          color: 'transparent',
-          zeroLineColor: 'transparent',
-        },
-        ticks: {
-          fontSize: 2,
-          fontColor: 'transparent',
-        },
-
-      }],
-    yAxes: [
-      {
-        display: false,
-        ticks: {
-          display: false,
-          
-        },
-      }],
-  },
-  elements: {
-    line: {
-      tension: 0.00001,
-      borderWidth: 1,
-    },
-    point: {
-      radius: 4,
-      hitRadius: 10,
-      hoverRadius: 4,
-    },
-  },
-};
-
-
-
-const cardChartOpts3 = {
-  tooltips: {
-    enabled: false,
-    // custom: CustomTooltips
-  },
-  maintainAspectRatio: false,
-  legend: {
-    display: false,
-  },
-  scales: {
-    xAxes: [
-      {
-        display: false,
-      }],
-    yAxes: [
-      {
-        display: false,
-      }],
-  },
-  elements: {
-    line: {
-      borderWidth: 2,
-    },
-    point: {
-      radius: 0,
-      hitRadius: 10,
-      hoverRadius: 4,
-    },
-  },
-};
-
-
-
-const cardChartOpts4 = {
-  tooltips: {
-    enabled: false,
-    // custom: CustomTooltips
-  },
-  maintainAspectRatio: false,
-  legend: {
-    display: false,
-  },
-  scales: {
-    xAxes: [
-      {
-        display: false,
-      }],
-    yAxes: [
-      {
-        display: false,
-      }],
-  },
-};
-
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-var elements = 27;
-var data1 = [];
-var data2 = [];
-var data3 = [];
-
-for (var i = 0; i <= elements; i++) {
-  data1.push(random(50, 200));
-  data2.push(random(80, 100));
-  data3.push(65);
-}
-
-
-
-class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
-
-    this.state = {
-      dropdownOpen: false,
-      radioSelected: 2,
-    };
-  }
-
-  toggle() {
-    this.setState({
-      dropdownOpen: !this.state.dropdownOpen,
-    });
-  }
-
-  onRadioBtnClick(radioSelected) {
-    this.setState({
-      radioSelected: radioSelected,
-    });
-  }
-
-  loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>
-
-  render() {
-    const { proyects } =Proyects
-    return (
-      <div className="animated fadeIn">
-        <Row>
-          <Col xs="12" sm="6" lg="3">
-            <Card color="primary" className="text-white ">
-              <CardBody className="pb-0">
-                <NavLink ></NavLink>
-               
-                <div className="text-value">{proyects.length}</div>
-                <div>Programas</div>
-              </CardBody>
-              <div className="chart-wrapper mx-3" style={{ height: '70px' }}>
-                <Line data="" options={cardChartOpts2} height={70} />
-              </div>
-            </Card>
-          </Col>
-
-          <Col xs="12" sm="6" lg="3">
-            <Card className="text-white bg-info">
-              <CardBody className="pb-0">
+  return (
+    <div className="animated fadeIn">
+      <Row>
+        <Col xs="12" sm="6" lg="3">
+          <Card color="primary" className="text-white ">
+            <CardBody className="pb-0">
               <NavLink ></NavLink>
-               
-                <div className="text-value">-</div>
-                <div>Beneficiarios</div>
-              </CardBody>
-              <div className="chart-wrapper mx-3" style={{ height: '70px' }}>
-                <Line data="" options={cardChartOpts1} height={70} />
-              </div>
-            </Card>
-          </Col>
 
-          <Col xs="12" sm="6" lg="3">
-            <Card className="text-white bg-warning">
-              <CardBody className="pb-0">
+              <div className="text-value">--</div>
+              <div>Obregon</div>
+            </CardBody>
+            <div className="chart-wrapper mx-3" style={{ height: '70px' }}>
+            </div>
+          </Card>
+        </Col>
+
+        <Col xs="12" sm="6" lg="3">
+          <Card className="text-white bg-info">
+            <CardBody className="pb-0">
               <NavLink ></NavLink>
-                
-                <div className="text-value">--</div>
-                <div>Alumnos</div>
-              </CardBody>
-              <div className="chart-wrapper" style={{ height: '70px' }}>
-                <Line data="" options={cardChartOpts3} height={70} />
-              </div>
-            </Card>
-          </Col>
 
-          <Col xs="12" sm="6" lg="3">
-            <Card className="text-white bg-success">
-              <CardBody className="pb-0">
+              <div className="text-value">--</div>
+              <div>Navojoa</div>
+            </CardBody>
+            <div className="chart-wrapper mx-3" style={{ height: '70px' }}>
+            </div>
+          </Card>
+        </Col>
+
+        <Col xs="12" sm="6" lg="3">
+          <Card className="text-white bg-warning">
+            <CardBody className="pb-0">
               <NavLink ></NavLink>
-                
-                <div className="text-value">--</div>
-                <div>Universidades</div>
-              </CardBody>
-              <div className="chart-wrapper mx-3" style={{ height: '70px' }}>
-                <Bar data="" options={cardChartOpts4} height={70} />
-              </div>
-            </Card>
-          </Col>
-        </Row>
 
-      </div>
-    );
-  }
+              <div className="text-value">--</div>
+              <div>Huatabampo</div>
+            </CardBody>
+            <div className="chart-wrapper" style={{ height: '70px' }}>
+            </div>
+          </Card>
+        </Col>
+      </Row>
+
+    </div>
+  );
 }
+// }
 
 export default Dashboard;
+
