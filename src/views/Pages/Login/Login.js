c60f2e547ec9b1bd93ed66b616dea6e044bc93fe
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2020-10-29 12:04:49 -0700

    componente de inicio de sesion, si el usuario tiene sesion activa lo redirige a la pantalla principal

--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -1,101 +1,122 @@
 import React, { useState } from 'react';
-import {Route } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { Card, CardBody, CardGroup, CardImg, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
 import img from "../Login/grameen.png";
+import PrivateRoute from '../../../containers/Private'
+import axiosClient from '../../../helpers/axiosClient'
+import { useAuth } from '../../../helpers/Helpers'
 
 const DefaultLayout = React.lazy(() => import('../../../containers/DefaultLayout'));
-const Login = (props) => {
-  const [user, saveuser] = useState({
-    username: "",
-    password: ""
-  });
-  // useEffect(() => {
-  //   if (logged) {
-  //     props.history.push('/proyects');
-  //   }      
 
-  // }, [msg, logged, mostrarAlerta, props.history])
-  const { username, password } = user;
-  const Log_in = e => {
+const Login = (props) => {
+  let history = useHistory();
+
+  const LayOut = (claims) => {
+    history.push('/grameen/inicio');
+    return <PrivateRoute path="/grameen/inicio" component={<DefaultLayout usuario={claims} />} />
+  }
+
+  const [user, saveuser] = useState({
+    password: "",
+    email: ""
+  });
+
+
+  // revisamos si ya hay una sesion abierta en el navegador
+  let { pending, claims } = useAuth();
+
+  if (pending) return false;
+  // si habia sesion y caduco continuar con el proceso de loggeo,
+  // de otra manera,redirige a la pantalla de inicio
+  if (claims !== 403) {
+    debugger
+    return LayOut(claims);
+  }
+
+
+  const { email, password } = user;
+
+  // funcion para iniciar sesion, envia los datos al backend para firmar el token
+  const LoginUser = async (e) => {
     e.preventDefault();
-    if (username.trim() === "" || password.trim() === "") {
+    if (email.trim() === "" || password.trim() === "") {
       return alert("Todos los campos son obligatorios");
-      // return false;
     }
-    else if (username === "demo" && password === "demo") {
-      // alert("Bienvenido")
-      window.localStorage.setItem('Demo', 'DEMO');
-      props.history.push('/grameen/inicio');
-        // eslint-disable-next-line no-unused-expressions
-        < Route path = "/grameen" name = "Home" render = { props => <DefaultLayout {...props} />} />
+    let login = await axiosClient.post("/api/signin", user);
+
+    if (login.data.info) {
+      let { data: { user } } = login
+      return LayOut(user);
     }
-    else {
-  alert("Datos Incorrectos");
-}
+
 
   }
-const _onChange = e => {
-  saveuser({
-    ...user,
-    [e.target.name]: e.target.value
-  })
-}
-return (
-  <div className="app flex-row align-items-center">
-    <Container>
-      <Row className="justify-content-center">
-        <Col md="8">
-          <CardGroup>
-            <Card className="p-4">
-              <CardBody className="align-items-center">
-                <Form onSubmit={Log_in}>
-                  <Row>
-                    <Col m="12" className="text-center">
-                      <h1>Iniciar Sesión</h1>
-                    </Col>
-                  </Row>
-                  <br />
-                  <InputGroup className="mb-3">
-                    <InputGroupAddon addonType="prepend">
-                      <InputGroupText>
-                        <i className="icon-user"></i>
-                      </InputGroupText>
-                    </InputGroupAddon>
-                    <Input type="text" name="username" value={username} placeholder="Usuario" id="username" onChange={_onChange} autoComplete="username" />
-                  </InputGroup>
-                  <InputGroup className="mb-4">
-                    <InputGroupAddon addonType="prepend">
-                      <InputGroupText>
-                        <i className="icon-lock"></i>
-                      </InputGroupText>
-                    </InputGroupAddon>
-                    <Input type="password" name="password" id="password" value={password} placeholder="Contraseña" onChange={_onChange} autoComplete="current-password" />
-                  </InputGroup>
-                  <Row>
-                    
-                    <CardBody className="text-center">
-                      <div>
-                        
-                        <button type="submit" className="btn btn-primary">Entrar</button>
-                      </div>
-                    </CardBody>
-                   
-                  </Row>
-                </Form>
-              </CardBody>
-            </Card>
-            <Card className="text-white d-md-down-none" style={{ width: '44%' }}>
-              <CardBody className="text-center">
-                <CardImg top width="10%" src={img} alt="Grameen AC" />
-                
-              </CardBody>
-            </Card>
-          </CardGroup>
-        </Col>
-      </Row>
-    </Container>
-  </div>
-);
+  const _onChange = e => {
+    e.preventDefault();
+    saveuser({
+      ...user,
+      [e.target.name]: e.target.value
+    })
+  }
+
+
+
+  return (
+    <div className="app flex-row align-items-center">
+      <Container>
+        <Row className="justify-content-center">
+          <Col md="8">
+            <CardGroup>
+              <Card className="p-4">
+                <CardBody className="align-items-center">
+                  <Form >
+                    <Row>
+                      <Col m="12" className="text-center">
+                        <h1>Iniciar Sesión</h1>
+                      </Col>
+                    </Row>
+                    <br />
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-user"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="text" name="email" value={email} placeholder="Usuario" id="email" onChange={e => _onChange(e)} autoComplete="email" />
+                    </InputGroup>
+                    <InputGroup className="mb-4">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-lock"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="password" name="password" id="password" value={password} placeholder="Contraseña" onChange={e => _onChange(e)} autoComplete="current-password" />
+                    </InputGroup>
+                    <Row>
+
+                      <CardBody className="text-center">
+                        <div>
+
+                          <button type="submit" className="btn btn-primary" onClick={e => LoginUser(e)}>Entrar</button>
+                        </div>
+                      </CardBody>
+
+                    </Row>
+                  </Form>
+                </CardBody>
+              </Card>
+              <Card className="text-white d-md-down-none" style={{ width: '44%' }}>
+                <CardBody className="text-center">
+                  <CardImg top width="10%" src={img} alt="Grameen AC" />
+
+                </CardBody>
+              </Card>
+            </CardGroup>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 // }
 
