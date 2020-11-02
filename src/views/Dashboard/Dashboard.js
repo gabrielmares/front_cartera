1d38ac40221e6ad4f3adaae700246e36e1694c7a
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2020-11-02 08:43:30 -0700

    limpieza de dependencias y componentes en vista principal

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -7,21 +7,41 @@ import {
   Row,
   NavLink
 } from 'reactstrap';
+import axiosClient from '../../helpers/axiosClient';
+import { usuarioContext } from '../../provider/contextUsers';
 
 
 
 const Dashboard = () => {
+  const { info } = React.useContext(usuarioContext)
+  const [obregon, setObregon] = React.useState({})
+  const [huatabampo, setHuatabampo] = React.useState({})
+  const [navojoa, setNavojoa] = React.useState({})
+
+  React.useEffect(() => {
+    axiosClient.get('/api/operaciones/totales', {
+      sucursal: info.sucursal
+    }).then(totales => {
+      console.log(totales.data.length)
+      setObregon(totales.data.filter(invoice => (parseInt(invoice.FINNOSUCURSAL) === 1)))
+      setHuatabampo(totales.data.filter(invoice => (parseInt(invoice.FINNOSUCURSAL) === 2)))
+      setNavojoa(totales.data.filter(invoice => (parseInt(invoice.FINNOSUCURSAL) === 3)))
+    })
+
+  }, [info])
 
   return (
     <div className="animated fadeIn">
       <Row>
         <Col xs="12" sm="6" lg="3">
-          <Card color="primary" className="text-white ">
+          <Card className="text-white bg-primary">
             <CardBody className="pb-0">
-              <NavLink ></NavLink>
 
-              <div className="text-value">--</div>
+
+              <NavLink ><div className="text-value">{obregon.length}</div></NavLink>
               <div>Obregon</div>
+
+
             </CardBody>
             <div className="chart-wrapper mx-3" style={{ height: '70px' }}>
             </div>
@@ -31,9 +51,9 @@ const Dashboard = () => {
         <Col xs="12" sm="6" lg="3">
           <Card className="text-white bg-info">
             <CardBody className="pb-0">
-              <NavLink ></NavLink>
+              <NavLink ><div className="text-value">{huatabampo.length}</div></NavLink>
 
-              <div className="text-value">--</div>
+
               <div>Navojoa</div>
             </CardBody>
             <div className="chart-wrapper mx-3" style={{ height: '70px' }}>
@@ -42,11 +62,11 @@ const Dashboard = () => {
         </Col>
 
         <Col xs="12" sm="6" lg="3">
-          <Card className="text-white bg-warning">
+          <Card className="text-white bg-success">
             <CardBody className="pb-0">
-              <NavLink ></NavLink>
+              <NavLink ><div className="text-value">{navojoa.length}</div></NavLink>
 
-              <div className="text-value">--</div>
+
               <div>Huatabampo</div>
             </CardBody>
             <div className="chart-wrapper" style={{ height: '70px' }}>
