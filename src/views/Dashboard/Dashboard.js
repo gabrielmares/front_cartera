3d90b81e2faf6f04ab9b7b9399a4029c5ee718cb
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2020-11-04 10:09:16 -0700

    filtrado de elementos por sucursal del usuario que hizo la solicitud al backend

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import {
 
   Card,
@@ -13,66 +14,93 @@ import { usuarioContext } from '../../provider/contextUsers';
 
 
 const Dashboard = () => {
-  const { info } = React.useContext(usuarioContext)
+  let history = useHistory();
+  const { info, setRequestBySuc } = React.useContext(usuarioContext)
   const [obregon, setObregon] = React.useState({})
   const [huatabampo, setHuatabampo] = React.useState({})
   const [navojoa, setNavojoa] = React.useState({})
-
+  const [loading, setLoading] = React.useState(true)
   React.useEffect(() => {
     axiosClient.get('/api/operaciones/totales', {
-      sucursal: info.sucursal
+      params: {
+        sucursal: info.sucursal
+      }
     }).then(totales => {
-      console.log(totales.data.length)
-      setObregon(totales.data.filter(invoice => (parseInt(invoice.FINNOSUCURSAL) === 1)))
-      setHuatabampo(totales.data.filter(invoice => (parseInt(invoice.FINNOSUCURSAL) === 2)))
-      setNavojoa(totales.data.filter(invoice => (parseInt(invoice.FINNOSUCURSAL) === 3)))
-    })
+      switch (info.sucursal) {
+        case 0:
+          setObregon(totales.data.filter(invoice => (parseInt(invoice.FINNOSUCURSAL) === 1)))
+          setHuatabampo(totales.data.filter(invoice => (parseInt(invoice.FINNOSUCURSAL) === 2)))
+          setNavojoa(totales.data.filter(invoice => (parseInt(invoice.FINNOSUCURSAL) === 3)))
+          break;
+        case 1:
+          setObregon(totales.data.filter(invoice => (parseInt(invoice.FINNOSUCURSAL) === 1)))
+          break;
+        case 2:
+          setHuatabampo(totales.data.filter(invoice => (parseInt(invoice.FINNOSUCURSAL) === 2)))
+          break;
+        case 3:
+          setNavojoa(totales.data.filter(invoice => (parseInt(invoice.FINNOSUCURSAL) === 3)))
+          break;
+        default:
+          break;
+      }
+      setLoading(false);
 
+    })
+    // eslint-disable-next-line
   }, [info])
+
+
+  if (loading) return false
+
+  // si da clic en el cuadro de sucursales, lo reenvia a la vista donde se muestra el total de solicitudes por sucursal
+  const routeTo = (lista) => {
+    console.log(lista.length)
+    setRequestBySuc(lista)
+    return history.push('/grameen/renovaciones');
+  }
+
+
+
 
   return (
     <div className="animated fadeIn">
       <Row>
-        <Col xs="12" sm="6" lg="3">
-          <Card className="text-white bg-primary">
+        {(obregon.length > 0) ? (<Col xs="12" sm="6" lg="3">
+          <Card className="text-white bg-primary " onClick={() => routeTo(obregon)} style={{ cursor: 'pointer' }}>
             <CardBody className="pb-0">
-
-
               <NavLink ><div className="text-value">{obregon.length}</div></NavLink>
               <div>Obregon</div>
-
-
             </CardBody>
             <div className="chart-wrapper mx-3" style={{ height: '70px' }}>
             </div>
           </Card>
-        </Col>
+        </Col>) : (null)}
 
-        <Col xs="12" sm="6" lg="3">
-          <Card className="text-white bg-info">
+        {(huatabampo.length > 0) ? (<Col xs="12" sm="6" lg="3" >
+          <Card className="text-white bg-success" onClick={() => routeTo(huatabampo)} style={{ cursor: 'pointer' }}>
             <CardBody className="pb-0">
               <NavLink ><div className="text-value">{huatabampo.length}</div></NavLink>
-
-
-              <div>Navojoa</div>
-            </CardBody>
-            <div className="chart-wrapper mx-3" style={{ height: '70px' }}>
-            </div>
-          </Card>
-        </Col>
-
-        <Col xs="12" sm="6" lg="3">
-          <Card className="text-white bg-success">
-            <CardBody className="pb-0">
-              <NavLink ><div className="text-value">{navojoa.length}</div></NavLink>
-
-
               <div>Huatabampo</div>
             </CardBody>
             <div className="chart-wrapper" style={{ height: '70px' }}>
             </div>
           </Card>
-        </Col>
+        </Col>) : (null)}
+
+
+        {(navojoa.length > 0) ? (<Col xs="12" sm="6" lg="3">
+          <Card className="text-white bg-info" onClick={() => routeTo(navojoa)} style={{ cursor: 'pointer' }}>
+            <CardBody className="pb-0">
+              <NavLink ><div className="text-value">{navojoa.length}</div></NavLink>
+              <div>Navojoa</div>
+            </CardBody>
+            <div className="chart-wrapper mx-3" style={{ height: '70px' }}>
+            </div>
+          </Card>
+        </Col>) : (null)}
+
+
       </Row>
 
     </div>
