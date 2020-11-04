a514fac17e3f23eaf13874d1741d551b38a8a7ba
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2020-11-04 10:08:05 -0700

    spinner de informacion cuando se genera la solicitud de credito y cuando se busca info en BD

---
+++ src/views/Notifications/Modals/SpinnerModal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Modal, ModalFooter, Spinner } from 'reactstrap';
+
+const SpinnerModal = ({ modal }) => {
+
+    setInterval(() => {
+        console.log('modal')
+    }, 3000);
+
+
+    return (
+        <div className="animated fadeIn justify-content-center">
+            <Modal isOpen={modal} size="sm" >
+                <div className="text-center">
+                    <Spinner
+                        className="text-center"
+                        color="info"
+                        style={{ width: '10rem', height: '10rem' }}
+                        type='grow'
+                    />
+                </div>
+                <h4 className="text-center">Generando solicitud...</h4>
+
+            </Modal>
+        </div>
+    );
+}
+
+export default SpinnerModal;
