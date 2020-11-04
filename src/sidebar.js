8f929ee64b302c44f7f6c745dc2e03de8d65cb3b
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2020-11-04 10:08:36 -0700

    rol de usuario a las rutas del sistema

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,69 +1,82 @@
 export default {
     items: [
         {
-            name: 'Inicio',
+            name: 'Principal',
             url: '/grameen/inicio',
             icon: "icon-home",
-
+            rol: 7
         },
+        // {
+        //     title: true,
+        //     name: 'Programas Sociales',
+        //     wrapper: {
+        //         element: '',
+        //         attributes: {},
+        //     },
+        // },
+
+        // {
+        //     name: "Programas",
+        //     url: "/grameen/programas",
+        //     icon: "icon-share"
+        // },
+
+        // {
+        //     divider: true,
+        // },
+
         {
             title: true,
-            name: 'Programas Sociales',
+            name: 'Creditos',
             wrapper: {
                 element: '',
                 attributes: {},
             },
-        },
-        {
-            name: "Beneficiarios",
-            url:"/grameen/beneficiarios",
-            icon:"icon-people"
-        },
-        {
-            name:"Programas",
-            url: "/grameen/programas",
-            icon:"icon-share"
-        },
-        
-        {
-            divider: true,
-        },
-       
-        {
-            title: true,
-            name: 'Vinculacion',
-            wrapper: {
-                element: '',
-                attributes: {},
-            },
+            rol: 7
         },
         {
             divider: true,
+            rol: 7
         },
         {
-            name: 'Universidades',
-            url: '/grameen/universidades',
-            icon: 'icon-graduation'
+            name: "Renovaciones",
+            url: "/grameen/renovaciones",
+            icon: "icon-star",
+            rol: 7
+            // children: [
+            //     {
+            //         name: 'Futuras',
+            //         url: '/grameen/renovaciones/futuras',
+            //         icon: 'icon-star'
+            //     },
+            // {
+            //     name: 'Procesadas',
+            //     url: '/grameen/renovaciones/procesadas',
+            //     icon: 'icon-star'
+
+            // },
+            //     ]
+            // },
+            // {
+            //     name: 'Universidades',
+            //     url: '/grameen/universidades',
+            //     icon: 'icon-graduation'
 
         },
         {
             divider: true,
+            rol: 7
         },
         {
-            name: 'Empresas',
-            url: '/grameen/empresas',
-            icon: 'icon-chart'
+            name: 'Usuarios',
+            url: '/grameen/icons',
+            icon: 'cui-people',
+            rol: 0
         },
-        {
-            name: 'Alumnos',
-            url: '/grameen/alumnos',
-            icon: 'icon-graduation'
+        // {
+        //     divider: true,
+        // },
 
-        },
-        {
-            divider: true,
-        },
-              
         {
             title: true,
             name: 'Administracion',
@@ -71,71 +84,76 @@ export default {
                 element: '',
                 attributes: {},
             },
+            rol: 2
         },
         {
             divider: true,
+            rol: 2
         },
-        // {
-        //     name: 'Roles y Usuarios',
-        //     url: '/grameen/admon',
-        //     icon: 'icon-note'
-        // }
-        // },
         {
-            name: 'Icons',
-            url: '/grameen/icons',
-            icon: 'icon-star',
-            children: [
-                {
-                    name: 'CoreUI Icons',
-                    url: '/grameen/icons/coreui-icons',
-                    icon: 'icon-star',
-                    badge: {
-                        variant: 'info',
-                        text: 'NEW',
-                    },
-                },
-                {
-                    name: 'Flags',
-                    url: '/grameen/icons/flags',
-                    icon: 'icon-star',
-                },
-                {
-                    name: 'Font Awesome',
-                    url: '/grameen/icons/font-awesome',
-                    icon: 'icon-star',
-                    badge: {
-                        variant: 'secondary',
-                        text: '4.7',
-                    },
-                },
-                {
-                    name: 'Simple Line Icons',
-                    url: '/grameen/icons/simple-line-icons',
-                    icon: 'icon-star',
-                },
-            ],
-        }
+            name: 'Roles y Usuarios',
+            url: '/grameen/admon/users',
+            icon: 'icon-note',
+            rol: 1
+        },
+        // },
+        // {
+        //     name: 'Icons',
+        //     url: '',
+        //     icon: 'icon-star',
+        //     children: [
+        //         {
+        //             name: 'CoreUI Icons',
+        //             url: '/grameen/icons/coreui-icons',
+        //             icon: 'icon-star',
+        //             badge: {
+        //                 variant: 'info',
+        //                 text: 'NEW',
+        //             },
+        //         },
+        //         {
+        //             name: 'Flags',
+        //             url: '/grameen/icons/flags',
+        //             icon: 'icon-star',
+        //         },
+        //         {
+        //             name: 'Font Awesome',
+        //             url: '/grameen/icons/font-awesome',
+        //             icon: 'icon-star',
+        //             badge: {
+        //                 variant: 'secondary',
+        //                 text: '4.7',
+        //             },
+        //         },
+        //         {
+        //             name: 'Simple Line Icons',
+        //             url: '/grameen/icons/simple-line-icons',
+        //             icon: 'icon-star',
+        //         },
+        //     ],
+        // }
         // {
         //   name: 'Charts',
-        //   url: '/charts',
+        //   url: '/grameen/icons',
         //   icon: 'icon-pie-chart',
         // }
         // // },
         // {
-        //   name: 'Icons',
-        //   url: '/icons',
-        //   icon: 'icon-star',
-        //   children: [
-        //     {
-        //       name: 'CoreUI Icons',
-        //       url: '/icons/coreui-icons',
-        //       icon: 'icon-star',
-        //       badge: {
-        //         variant: 'info',
-        //         text: 'NEW',
-        //       },
-        //     },
+        //     name: 'Icons',
+        //     url: '/grameen/iconos',
+        //     icon: 'icon-star',
+        //     children: [
+        //         {
+        //             name: 'CoreUI Icons',
+        //             url: '/icons/coreui-icons',
+        //             icon: 'icon-star',
+        //             badge: {
+        //                 variant: 'info',
+        //                 text: 'NEW',
+        //             },
+        //         }
+        //     ]
+        // }
         //     {
         //       name: 'Flags',
         //       url: '/icons/flags',
