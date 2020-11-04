addb6597738e6df811afdbd45854cdc47d6838eb
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2020-11-04 14:21:26 -0700

    modal de notificacion para el usuario, de la generacion del documento

--- src/views/Solicitudes/RequestTable.js
+++ src/views/Solicitudes/RequestTable.js
@@ -3,21 +3,27 @@ import { Table } from 'reactstrap'
 import axios from 'axios'
 import generateDoc from '../../helpers/GeneratorDocs'
 
-const RequestTable = ({ info }) => {
-
+const RequestTable = ({ info, show, hide }) => {
 
     const handlePrint = async CODIGO => {
-        const nuevaSolicitud = await axios.get(`${process.env.REACT_APP_SERVIDOR}/api/operaciones/renovacion`, {
-            withCredentials: true,
-            params: {
-                CODIGO
-            },
-        })
-        generateDoc(nuevaSolicitud.data);
+        hide(!show)
+        try {
+            const nuevaSolicitud = await axios.get(`${process.env.REACT_APP_SERVIDOR}/api/operaciones/renovacion`, {
+                withCredentials: true,
+                params: {
+                    CODIGO
+                },
+            })
+            generateDoc(nuevaSolicitud.data).then(() => hide(false));
+        } catch (error) {
+            console.log(error)
+            alert('sucedio un error al generar el archivo')
+            hide(!show)
+        }
     }
     return (
         <>
-            <Table size='lg' responsive striped className="justify-content-center tablarenovaciones" >
+            <Table size='lg' responsive striped className="justify-content-center tablarenovaciones" aria-disabled={show}>
                 <thead className="text-center" style={{ position: 'sticky', top: 0, backgroundColor: '#fff' }}>
                     <tr>
                         <th style={{ width: '18rem' }}>Nombre</th>
@@ -40,7 +46,7 @@ const RequestTable = ({ info }) => {
                             </td>
                             <td style={{ width: '4rem' }}>{cliente.CENTRO}</td>
                             <td style={{ width: '4rem' }}>{cliente.GRUPO}</td>
-                            <td style={{ width: '8rem' }}>{cliente.CONTRATO}, {(cliente.SALDO).toFixed(2)}</td>
+                            <td style={{ width: '9rem' }}>{cliente.CONTRATO}, ${(cliente.SALDO).toFixed(2)}</td>
                             <td style={{ width: '6rem' }}>{(cliente.PORPAGADO).toFixed(2)} %</td>
                             <td style={{ width: '9rem' }}>{cliente.ULTIMO}</td>
                             <td style={{ width: '9rem' }}>{cliente.VENCIMIENTO}</td>
