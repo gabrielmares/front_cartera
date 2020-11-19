cecc8a8e8fc984861b0e320e93b97040c10ca8ca
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2020-11-19 11:08:41 -0700

    manejador de rutas privadas que revisa cuando se recarga la app o se inicia sesion

--- src/containers/Private.js
+++ src/containers/Private.js
@@ -5,20 +5,33 @@ import { usuarioContext } from '../provider/contextUsers'
 import Login from '../views/Pages/Login/Login';
 
 const PrivateRoute = ({ component: Component }) => {
-
     const { info, setInfo } = useContext(usuarioContext);
-
+    //si hay informacion de la sesion de usuario, useAuth no se 
+    //hace una nueva verificacion de identidad
+    // useAuth se usa cuando el usuario recarga la app, verifica con el backend
+    // la identidad del usuario que tiene abierta la sesion
     let q = useAuth(info);
     const { pending, claims } = q
 
-
+    // cuando aun no tiene la informacion useAuth responde en true
+    // cuando termina la consulta retorna false y la informacion del usuario
+    
     if (pending) return false;
-
+    // cuando la sesion caduco o el token no es valido el servidor retorna
+    // el msg 403, sirve de indicacion a la app para reenviar al usuario
+    // a iniciar sesion de nuevo
     if (claims === 403) return <Redirect to="/" component={Login} />
-    let data = (!info) ? (claims) : (claims)
-    setInfo(data)
+
+
+    // cuando refresca la app se le reasignan los claims a info
+    // el state global, para manejar la informacion del usuario
+    if (!info) {
+        setInfo(claims)
+    }
+
+
     return (
-        <Component usuario={data} />
+        <Component usuario={info} />
     );
 }
 
