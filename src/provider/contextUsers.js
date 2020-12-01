2e3b868fcfb96794818359d05b704fc316f049ea
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2020-12-01 08:29:43 -0700

    manejo del state global de la app

--- src/provider/contextUsers.js
+++ src/provider/contextUsers.js
@@ -3,11 +3,18 @@ import React, { createContext, useState } from 'react';
 export const usuarioContext = createContext();
 
 const UsersContext = (props) => {
-    let [info, setInfo] = useState(null)
-    let [requestBySuc, setRequestBySuc] = useState({})
+    let [info, setInfo] = useState(null) //almacen de la informacion del usuario, permisos, restricciones y configuracion de usuario
+    let [requestBySuc, setRequestBySuc] = useState([]); //state para almacenar las solicitudes de la vista de solicitudes
+    let [renovations, setRenovations] = useState([]); // state que almacena las renovaciones posibles que se despliegan en la tabla de la vista
     let [mngtUser, setMngtUser] = useState(null)
-
-
+    // almacenamiento del filtro definido por el usuario, al usar el formulario de las vistas
+    let [form, setForm] = useState({
+        FINNOSUCURSAL: "",
+        centro: '',
+        from: "",
+        to: "",
+    })
+    let [spin, setSpin] = useState(false)
 
     return (
         <usuarioContext.Provider
@@ -17,7 +24,13 @@ const UsersContext = (props) => {
                 mngtUser,
                 setMngtUser,
                 requestBySuc,
-                setRequestBySuc
+                setRequestBySuc,
+                form,
+                setForm,
+                spin,
+                setSpin,
+                renovations,
+                setRenovations
             }}
         >
             {props.children}
